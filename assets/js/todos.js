783dfd72bfcdc7df6a44dbf8df06b15a36b6015d
Alex Ference <alexference23@gmail.com>
2020-01-14 22:52:16 -0500

    first commit

---
+++ assets/js/todos.js
@@ -0,0 +1,33 @@
+$("ul").on("click","li", function(){
+	$(this).toggleClass("done");
+	
+});
+
+$("ul").on("click","span", function(event){
+	$(this).parent().fadeOut(500,function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
+
+
+$("input[type='text']").keypress(function(event){
+	if (event.which === 13){
+		//get new text
+		var todoText = $(this).val();
+		$(this).val("");
+
+		//VERY KEYYY DO TOMORROW
+		//create new li and add to ul
+		$("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText+"</li>")		
+	}
+
+	
+});
+
+$(".fa-plus").click(function(){
+	$("input[type='text']").fadeToggle();
+
+});
+
+
